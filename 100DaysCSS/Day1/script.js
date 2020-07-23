b5315937056a656b9f3719d50d7b1d02e9cafeca
c-o-z <cozartharrison@gmail.com>
2020-07-23 18:42:51 -0400

    added jquery line8

--- 100DaysCSS/Day1/script.js
+++ 100DaysCSS/Day1/script.js
@@ -1,4 +1,6 @@
-$('.menu-icon').on('click', function() {
-    $(this).toggleClass('active');
-    $(this).find('div').removeClass('no-animation');
-});
+$(function() {
+    $('.menu-icon').on('click', function() {
+        $(this).toggleClass('active');
+        $(this).find('div').removeClass('no-animation');
+    });
+  });
