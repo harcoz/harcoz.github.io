6f84a6bf66d10fe449fcfa0837dd6dcb45dbf69f
c-o-z <cozartharrison@gmail.com>
2020-07-23 18:22:54 -0400

    added jquery line5

--- 100DaysCSS/Day1/script.js
+++ 100DaysCSS/Day1/script.js
@@ -1,4 +1,3 @@
-<script> src = "https://100dayscss.com/codepen/js/jquery.min.js"</script>
 $('.menu-icon').bind('click', function() {
     $(this).toggleClass('active');
     $(this).find('div').removeClass('no-animation');
